32784bbbc7d3218ec2d56632f69fff3bda92282d
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-07-11 18:00:22 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  try {
+    const apiURL = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    return data;  
+  } catch (error) {
+    return new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
@@ -7,3 +14,31 @@ if (typeof module !== 'undefined') {
     fetchProducts,
   };
 }
+
+// id
+// site_id
+// title
+// seller
+// price
+// currency_id
+// available_quantity
+// sold_quantity
+// buying_mode
+// listing_type_id
+// stop_time
+// condition
+// permalink
+// thumbnail
+// accepts_mercadopago
+// installments
+// address
+// shipping
+// seller_address
+// attributes
+// differential_pricing
+// original_price
+// category_id
+// official_store_id
+// catalog_product_id
+// tags
+// catalog_listing
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,33 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('checks if the fetchProducts function is indeed a function', () => {
+    expect.assertions(1);
+    expect(typeof(fetchProducts)).toEqual('function');
+  });
+
+  it('checks if "fetch" was called', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('check if you are using the correct endpoint', async () => {
+    expect.assertions(1);
+    const apiURL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(apiURL);
+  });
+
+  it('check if the data structure of the returned object is the same as the structure of "computerSearch"', async () => {
+    expect.assertions(1);
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+
+  it('checks if when calling the function without parameter, an error is displayed', async () => {
+    expect.assertions(1);
+    const error = 'You must provide an url';
+    expect(await fetchProducts()).toEqual(new Error(error));      
+  });
+
 });
